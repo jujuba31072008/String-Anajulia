7c80b87aae07f8ca487355f6200c364ed8308b29
jujuba31072008 <ana.barz.santos@escola.pr.gov.br>
2025-06-24 15:16:42 +0000

    4 commit

--- script.js
+++ script.js
@@ -5,18 +5,32 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
+    
     const palavrasChave = processaTexto(texto);
-
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
+ const frequencias = contaFrequencias(palavras);
+
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
     let frequencias = {};
+
     for (let i of palavras) {
         frequencias[i] = 0;
-        for (let j of palavras) {
+
+    for (let j of palavras) {
             if (i == j) {
                 frequencias[i]++;
             }
@@ -25,5 +39,6 @@ function processaTexto(texto) {
 
     console.log(frequencias);
 
-    return palavras;
+    return frequencias;
+
 }
