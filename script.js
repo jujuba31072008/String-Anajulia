16ffee780bf4af00fb5616bf9fb48c86f7a7055e
jujuba31072008 <ana.barz.santos@escola.pr.gov.br>
2025-07-01 15:00:40 +0000

    Resolvendo bugs.

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector('#entrada-de-texto').value;
-    const campoResultado = document.querySelector('#resultado-palavrachave');
-    
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
     const palavrasChave = processaTexto(texto);
+
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
- const frequencias = contaFrequencias(palavras);
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
 
-  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    palavras = tiraPalavrasRuins(palavras);
 
-  function ordenaPalavra(p1, p2) {
-    return frequencias[p2] - frequencias[p1];
-  }
+    const frequencias = contaFrequencias(palavras);
 
-  return ordenadas.slice(0, 10);
+    // Ordena a palavra por frequência
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
+
+function tiraPalavrasRuins(palavras) {
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
 
 function contaFrequencias(palavras) {
-    let frequencias = {};
+    const frequencias = {};
 
     for (let i of palavras) {
         frequencias[i] = 0;
-
-    for (let j of palavras) {
+        for (let j of palavras) {
             if (i == j) {
                 frequencias[i]++;
             }
         }
     }
 
-    console.log(frequencias);
-
     return frequencias;
-
 }
